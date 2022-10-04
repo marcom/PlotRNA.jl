12f2da6c1de45d05e12bbcd589602fa51af34cf2
Marco Matthies <marco.matthies@gmail.com>
2022-10-04 18:37:04 +0200

    add SnoopPrecompile statements

--- src/PlotRNA.jl
+++ src/PlotRNA.jl
@@ -1,8 +1,17 @@
 module PlotRNA
 
+using SnoopPrecompile: @precompile_all_calls
+
 export VARNA, plot_structure
 
 include("plot_structure.jl")
 include("varna.jl")
+
+@precompile_all_calls begin
+    plot_structure("(((...)))")
+    plot_structure("(((...)))"; sequence="GGGAAACCC")
+    plot_structure_makie("(((...)))")
+    plot_structure_makie("(((...)))"; sequence="GGGAAACCC")
+end
 
 end # module PlotRNA
