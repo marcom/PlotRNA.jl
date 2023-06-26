a8f35e0a391f54467df199b48115c40231cbf1cf
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-06-26 16:22:05 +0200

    Do not run some tests from Aqua.test_all that fail on julia-1.6

--- test/aqua.jl
+++ test/aqua.jl
@@ -3,5 +3,13 @@ using PlotRNA
 
 @testset "Aqua.test_all" begin
     showtestset()
-    Aqua.test_all(PlotRNA)
+    if VERSION >= v"1.7"
+        Aqua.test_all(PlotRNA)
+    else
+        # Aqua-0.6.4 on julia-1.6 complains about method ambiguities
+        # and Project.toml formatting, julia-1.9 does not
+        # Ref for Project.toml formatting: https://github.com/JuliaTesting/Aqua.jl/issues/105
+        # Method ambiguities might be due to the compiler in julia-1.6 being a lot older
+        Aqua.test_all(PlotRNA; ambiguities=false, project_toml_formatting=false)
+    end
 end
